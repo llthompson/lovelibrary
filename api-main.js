199300fa49f232eefc53c38df151b7b2123de154
llthompson <lisalacythompson+github@gmail.com>
2023-08-13 11:49:31 -0500

    initial search function

--- api-main.js
+++ api-main.js
@@ -4,6 +4,40 @@
 // this.setState({ books: result.items})
 // })}
 
-async function fetchBooks() {
-    const response = await fetch('https://www.googleapis.com/books/v1/volumes?q=search-terms&key=')
+// search book semiosis
+
+const button = document.getElementById('btn');
+const input = document.getElementById('input');
+const ul = document.getElementById('items');
+
+let arrayOfBooks = [];
+
+
+button.addEventListener('click', () => {
+
+    let search = input.value;
+    fetchBooks(search);
+
+})
+
+
+async function fetchBooks(search) {
+    const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${apiKey}`);
+    const data = await response.json();
+    arrayOfBooks = data.items;
+    displayBooks();
 }
+
+const displayBooks = () => {
+    ul.innerHTML = '';
+    arrayOfBooks.forEach(book => {
+        const li = document.createElement('li');
+        li.className = 'bookItem';
+        li.textContent = book.volumeInfo.title;
+        // console.log(book.volumeInfo);
+        ul.appendChild(li);
+    });
+
+}
+
+console.log(fetchBooks)
